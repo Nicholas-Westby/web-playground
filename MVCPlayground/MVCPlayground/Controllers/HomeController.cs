169e6e901e945db4bec08124227864bf835bc8df
dbaker <dbaker@STELLA>
2016-12-07 04:11:47 +1100

    attempt to use the database in the view

--- MVCPlayground/MVCPlayground/Controllers/HomeController.cs
+++ MVCPlayground/MVCPlayground/Controllers/HomeController.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MVCPlayground.Database;
 
 namespace MVCPlayground.Controllers
 {
     public class HomeController : Controller
     {
+
+        NicksDatabaseEntities db = new NicksDatabaseEntities();
+        
         public ActionResult Index()
         {
-            return View();
+            return View(db.NicksFavoriteThings);
         }
 
         public ActionResult About()
